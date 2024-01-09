b531ae8f7539a275203b66b2a63a705c653eede0
sumit <sumitchauhan1080@gmail.com>
2024-01-09 12:07:38 +0530

    added some more

--- springboot-search-rest-api/src/main/java/net/javaguides/springboot/SpringbootSearchRestApiApplication.java
+++ springboot-search-rest-api/src/main/java/net/javaguides/springboot/SpringbootSearchRestApiApplication.java
@@ -1,9 +1,12 @@
 package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"net.javaguides.springboot.entity"})
 public class SpringbootSearchRestApiApplication {
 
 	public static void main(String[] args) {
